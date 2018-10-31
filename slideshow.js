6316aeb76663ba58b5e164ccbd3ba97585644d82
tubale <43082103+tubale@users.noreply.github.com>
2018-10-30 21:18:51 -0700

    Update slideshow.js

--- slideshow.js
+++ slideshow.js
@@ -1,11 +1,10 @@
-
-$('#slideshow > div:gt(0)').hide();
+var current = 0,
+    slides = document.getElementsByTagName("img");
 
 setInterval(function() {
-  $('#slideshow > div:first')
-    .fadeOut(1000)
-    .next()
-    .fadeIn(1000)
-    .end()
-    .appendTo('#slideshow');
-}, 3000)
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0;
+  }
+  current = (current != slides.length - 1) ? current + 1 : 0;
+  slides[current].style.opacity = 1;
+}, 3000);
